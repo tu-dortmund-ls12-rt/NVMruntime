b6b0fce3c3cdec30ac4da674b799fdfe1cdd2be6
Christian Hakert <christian.hakert@tu-dortmund.de>
2019-10-31 08:09:28 +0100

    cleanup build process

--- application/test_app.cpp
+++ application/test_app.cpp
@@ -1,8 +1,7 @@
+#include <system/syscall_interface.h>
 void run_disjkstra();
 
 void app_init() {
-    asm volatile("svc #10");
-
     run_disjkstra();
-    asm volatile("svc #0");
+    syscall_shutdown();
 }
